6eaa0369c45ef09dc627964d61aac83791df52d6
Foste380 <foste380.publique@gmail.com>
2026-07-18 22:58:44 +0200

    [Add] Player Attack script
    Variable liste
    OnAwake
    Action Empty

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -3,5 +3,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [SerializeField] private float AttAnimDelay;
+    [SerializeField] private float ReuseDelay;
+    [SerializeField] private float useDelay;
+
+    private RigidBody RB;
+    private Collider AttZone;
+    private InputSystem AttInput;
+    private bool CanAtt = true;
     
+    private void OnAwake()
+    {
+        
+        RB = GetComponent<RigidBody>();
+        AttInput = InputSystem.ation.FindAction("Attack");
+        AttInput.started = Action;
+
+        if (AttInput != null)
+        {
+            Debug.Log("Player_AttInput INITIALISE");
+        }
+        else
+        {
+            Debug.Log("Payer_AttInput NULL");
+        }
+    }
+
+    private void Action(InputAction.CallbackContext context)
+    {
+        if (CanAtt)
+        {
+            
+        }
+    }
 }
