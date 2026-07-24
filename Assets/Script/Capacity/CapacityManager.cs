2369a3636c4bb1a89a3bb49f5cc7bfab5943afea
Foste380 <foste380.publique@gmail.com>
2026-07-24 17:04:30 +0200

    [Update] Finalyse Wall Slide V1
    - Shape Script finalise
    - Wall Slide finalise
    - Move script update

--- Assets/Script/Capacity/CapacityManager.cs
+++ Assets/Script/Capacity/CapacityManager.cs
@@ -3,22 +3,31 @@ using UnityEngine.InputSystem;
 
 public class CapacityManager : MonoBehaviour
 {
+    private PlayerMove Move;
+
     //Wall Slide
     private InputAction WSInput;
     private bool WSActive = false;
     private PlayerShapeManager ShapeManager;
+    public bool OnWall = false;
+    private bool WallHere = false;
 
     //Setup
     private void Awake()
     {
+        ShapeManager = GetComponent<PlayerShapeManager>();
+        Move = GetComponent<PlayerMove>();
+
         WSInput = InputSystem.actions.FindAction("Wall Slide");
+        WSInput.started += WSStarted;
+        WSInput.canceled += WSEnded;
 
         AwakeDbg();
     }
 
     private void Update()
     {
-        WallSlide();
+        WSShape();
     }
 
     //Setup Debug
@@ -35,19 +44,61 @@ public class CapacityManager : MonoBehaviour
     }
 
     //Action
-    private void WallSlide()
+    private void WSShape()
     {
-        if (WSInput.triggered)
+        //Change Shape (quand on vient d'appuyer)
+        if (WSInput.WasPressedThisFrame() && WallHere)
         {
             WSActive = true;
             Debug.Log("Wall_Slide_Status : ACTIVE");
             ShapeManager.SetNewShape(ShapeManager.WSMesh, ShapeManager.WSCollider);
         }
-        else
+
+        //Quand on relâche la touche (ou qu'on n'est plus sur le mur)
+        if (WSInput.WasReleasedThisFrame() || !WallHere)
         {
-            WSActive = false;
-            Debug.Log("Wall_Slide_Status : INACTIVE");
-            ShapeManager.SetNewShape(ShapeManager.InitialeMesh, ShapeManager.InitialCollider);
+            if (WSActive)
+            {
+                WSActive = false;
+                Debug.Log("Wall_Slide_Status : INACTIVE");
+                ShapeManager.SetNewShape(ShapeManager.InitialeMesh, ShapeManager.InitialCollider);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Wall Slide"))
+        {
+            WallHere = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Wall Slide"))
+        {
+            WallHere = false;
+            Move.RB.useGravity = true;
+            OnWall = false;
+        }
+    }
+
+    private void WSStarted(InputAction.CallbackContext context)
+    {
+        if (WallHere)
+        {
+            Move.RB.useGravity = false;
+            OnWall = true;
+        }
+    }
+
+    private void WSEnded(InputAction.CallbackContext context)
+    {
+        if (OnWall)
+        {
+            Move.RB.useGravity = true;
+            OnWall = false;
         }
     }
 }
