cc1aa12916db34d902cc40eb96c09923fe77afcd
Foste380 <foste380.publique@gmail.com>
2026-07-27 13:57:02 +0200

    [Add] Game Manager
    - Create Game Manager Object
    - Create Game Manager Prefap
    - Create GameManager Script [Unfinish]

---
+++ Assets/Script/GameObject.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameObject : MonoBehaviour
+{
+    public int color = 1;
+
+    [SerializeField] private int NbColor = 2;
+
+    private string Txtcolor;
+
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
