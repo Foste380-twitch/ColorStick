de6c6d0b262b5c64e6d5b4b73582795a0e6521ef
Foste380 <foste380.publique@gmail.com>
2026-07-30 09:44:50 +0200

    [Add] Begin Color UI

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 
-public class GameObject : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public int color = 1;
 
     [SerializeField] private int NbColor = 2;
 
     private string Txtcolor;
-
 
     void Start()
     {
